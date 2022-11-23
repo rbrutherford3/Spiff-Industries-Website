6804f4a9a1502d1d153eb33f936e7bef81c6b2d3
Robert Rutherford <rbrutherford3@gmail.com>
2022-11-23 21:25:05 +0000

    Configuring for subpath in server

--- spiffindustries/urls.py
+++ spiffindustries/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('taskmaster.urls')),
+    path('taskmaster/', include('taskmaster.urls')),
     path('admin/', admin.site.urls),
 ]
